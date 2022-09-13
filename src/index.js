cf2d5e941a9f7614b43ce46437fae78a8a24d9b1
Mike <nux7jr@gmail.com>
2022-09-13 17:50:56 +0700

    1.2
    +-

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const slider = () => {
   for (slide of slides) {
     slide.addEventListener("click", (slide) => {
       clearClass();
-      console.log(slide);
-      console.log(slide.target);
+      // console.log(slide.target);
       slide.target.classList.add("active");
     });
   }
@@ -15,3 +14,21 @@ const slider = () => {
   }
 };
 slider();
+
+const popUp = () => {
+  const slidesBtns = document.querySelectorAll(".slide__button");
+  for (btn of slidesBtns) {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const popUp = document.querySelector(".popUp");
+      popUp.classList.add("popUp--active");
+    });
+  }
+  const closeBtn = document.querySelector(".popUp__closeButton");
+  closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const popUp = document.querySelector(".popUp");
+    popUp.classList.remove("popUp--active");
+  });
+};
+popUp();
